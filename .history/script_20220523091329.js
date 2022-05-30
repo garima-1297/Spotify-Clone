a5bbef25e2a214c4a75ac8fbab6c25237dc16a71
garima-1297 <82163615+garima-1297@users.noreply.github.com>
2022-05-30 10:04:32 +0530

    Delete .history directory

--- .history/script_20220523091329.js
+++
@@ -1,9 +0,0 @@
-console.log("Welcome to spotify");
-let songs = [
-    {songName: "Let me Love You", filePath: "", coverPath: "covers/1.jpg"},
-    {songName: "Let me Love You", filePath: "", coverPath: "covers/1.jpg"},
-    {songName: "Let me Love You", filePath: "", coverPath: "covers/1.jpg"},
-    {songName: "Let me Love You", filePath: "", coverPath: "covers/1.jpg"},
-    {songName: "Let me Love You", filePath: "", coverPath: "covers/1.jpg"},
-    {songName: "Let me Love You", filePath: "", coverPath: "covers/1.jpg"},
-]
